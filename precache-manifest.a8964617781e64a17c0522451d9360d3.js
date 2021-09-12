ecda95df8d578953682b88397797bad558078e46
madelinekreiff <madelinekreiff@gmail.com>
2021-09-11 21:19:56 -0700

    Deployment of CodeSandbox code

---
+++ precache-manifest.a8964617781e64a17c0522451d9360d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ad299ed6a05103ca001",
+    "url": "https://.github.io/csb-1y58k/static/css/main.72578075.chunk.css"
+  },
+  {
+    "revision": "8ad299ed6a05103ca001",
+    "url": "https://.github.io/csb-1y58k/static/js/main.f9dad6e3.chunk.js"
+  },
+  {
+    "revision": "a8a75b51bf000e53bd18",
+    "url": "https://.github.io/csb-1y58k/static/js/runtime~main.ccc95e14.js"
+  },
+  {
+    "revision": "4f1e19e6e07ead45ea90",
+    "url": "https://.github.io/csb-1y58k/static/js/2.f3d7e44e.chunk.js"
+  },
+  {
+    "revision": "1e8af972ec1f22fa49e5d0df2334b225",
+    "url": "https://.github.io/csb-1y58k/index.html"
+  }
+];
